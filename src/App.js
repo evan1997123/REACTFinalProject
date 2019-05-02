9a40030e237734ab00ce0aa0389bccb1388fe566
Evan Sum <evansum@airbears2-10-142-166-93.airbears2.1918.berkeley.edu>
2019-05-02 14:12:26 -0700

    front page image

--- src/App.js
+++ src/App.js
@@ -32,6 +32,9 @@ class App extends React.Component {
         </Navbar>
         {/* <Link to="/pugs">See Pugs</Link> */}
         {/* <Link to="/counselors">See Counselors</Link> */}
+        <div className="home">
+          <img src="http://getwallpapers.com/wallpaper/full/d/6/c/309423.jpg" />
+        </div>
         <Route path="/counselors" component={CardHolder} />
         <Route path="/pugs" component={PugHolder} />
         <Route path="/symptoms" component={SymptomsHolder}/>
