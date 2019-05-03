4d0e88d5af26b9ed8fd16f85b03f2bd99c89f46c
Christine Luo <christineluo@berkeley.edu>
2019-05-02 18:04:52 -0700

    Final Touch Done

--- src/App.js
+++ src/App.js
@@ -12,25 +12,27 @@ import Home from './Home';
 class App extends React.Component {
   render() {
     return (
-      <div className="navbar-header">
-        <Navbar expand="lg">
-        <Navbar.Brand href="/">Andy's Website</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-        <Nav.Link href="/pugs">See Pugs</Nav.Link>
-        <Nav.Link href="/counselors">See Counselors</Nav.Link>
-        <Nav.Link href="/symptoms">See Symptoms</Nav.Link>
-        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
-        {/* </NavDropdown> */}
-        </Nav>
-        </Navbar.Collapse>
-        </Navbar>
+      <div>
+        <div className="navbar-header">
+          <Navbar expand="lg">
+          <Navbar.Brand href="/">Andy's Website</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+          <Nav.Link href="/pugs">See Pugs</Nav.Link>
+          <Nav.Link href="/counselors">See Counselors</Nav.Link>
+          <Nav.Link href="/symptoms">See Symptoms</Nav.Link>
+          {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+          {/* </NavDropdown> */}
+          </Nav>
+          </Navbar.Collapse>
+          </Navbar>
+        </div>
         {/* <Link to="/pugs">See Pugs</Link> */}
         {/* <Link to="/counselors">See Counselors</Link> */}
         
